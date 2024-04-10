730374c59ad246448763673cb484bf87f597f2bb
hydrotolerance <julzhafiz@gmail.com>
2024-04-10 12:57:13 +0800

    (Add) Image and also Prototype Description

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,24 +5,24 @@ import { RxHamburgerMenu } from "react-icons/rx";
 function Header() {
   return (
     <>
-      <div className="bg-red-500 text-white py-5 max-sm:hidden">
+      <div className=" py-5 max-sm:hidden">
         <div className="flex justify-between mx-5">
           <ul>
             <li className="px-3">asdfasdf</li>
           </ul>
           <ul className="flex">
             <li className="mx-3">
-              <Link to={"/home"}>Home</Link>
+              <Link to={"/home"}>HOME</Link>
             </li>
             <li className="mx-3">
-              <Link to={"/about"}>About</Link>
+              <Link to={"/about"}>ABOUT</Link>
             </li>
             <li className="mx-3">
-              <Link to={"/project"}>Project</Link>
+              <Link to={"/project"}>PROJECT</Link>
             </li>
           </ul>
           <ul className="flex">
-            <li className="border px-3 rounded-full border-white">
+            <li className="border px-3 rounded-full ">
               Download CV
             </li>
           </ul>
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,21 +1,48 @@
 import React from "react";
 import Layout from "../../layout/Layout";
 import ProfileImage from "../../assets/Arola_Image.webp";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Home() {
   return (
     <>
       <Layout>
-        <div className="flex justify-center items-center h-96">
-          <div className="text-center">
-            <div>
-              HELLO I AM 
+        <div className="flex justify-center items-center bg-red">
+          <div className="gap-10 md:grid md:grid-cols-2 mt-20">
+            <div className="">
+              <div>
+                <img src={ProfileImage} alt="" className="rounded-full h-48 w-48 mb-5" />
+              </div>
+              <div className="w-[23rem] max-lg:w-[20rem]">
+                <div className="mb-2 font-bold max-sm:mx-auto">
+                  HI, Im Mohammad Jul Hafiz Arola
+                </div>
+                <div className="mb-2">
+                  A software engineer with a passion in ui and front end development
+                </div>
+                <div className="flex">
+                    <FaGithub className="text-2xl me-3"/>
+                    <FaLinkedin className="text-2xl"/>
+                </div>
+              </div>
             </div>
-            <div>
-              MOHAMMAD JUL HAFIZ AROLA
+            <div className="w-[25rem] max-lg:w-[20rem]">
+              <div className=" text-wrap">
+                <div className="mb-2">
+                  Computer science graduate with a minor in cybersecurity. My interest in web development sparked during college, where I discovered a passion for building functional web applications.
+                </div>
+                <div className="mb-2">
+                  Since college, I've gained valuable experience through freelance work, focusing on building web applications that prioritize functionality. This hands-on experience has allowed me to refine my skills and deepen my understanding of various technologies.
+                </div>
+                <div className="mb-2">
+                  Continuously seeking to enhance my skills, I work on personal projects using a range of technologies. These projects not only serve as a creative outlet but also as a means to stay up-to-date with the latest trends and tools in web development.
+                </div>
+              </div>
             </div>
+
           </div>
-          </div>
+          
+        </div>
       </Layout>
     </>
   );
