35d93c4d9e9155e161fd3de3d4031b69b43f77b5
hydrotolerance <julzhafiz@gmail.com>
2024-05-06 14:04:44 +0800

    (Fix) The Profile Components

--- src/components/Home_Components/Profile_Components/index.tsx
+++ src/components/Home_Components/Profile_Components/index.tsx
@@ -1,48 +1,54 @@
 import ProfileImage from "../../../assets/Arola_Image.webp";
-import { FaGithub, FaLinkedin, } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaInstagram, FaTwitter } from "react-icons/fa";
 function Profile() {
   return (
     <div className="flex justify-center items-center ">
             <div className="md:w-[750px] xl:w-[800px] mt-7">
-            <div className="gap-10 sm:grid sm:grid-cols-2 mx-2 max-md:mx-4">
-              <div className="flex flex-col justify-center">
-                <div className="max-md:flex max-md:justify-center">
+            <div className="flex flex-wrap max-md:justify-center space-x-10 mx-4">
+              <div className="flex-wrap">
+                <div className="flex justify-center">
                   <img
                     src={ProfileImage}
                     alt=""
                     className="rounded-full h-48 w-48 mb-5"
                   />
                 </div>
-                <div className="">
-                  <div className="mb-2 font-bold text-xl max-md:text-center">
-                    HI, Im Mohammad Jul Hafiz Arola
-                  </div>
-                  
-                  <div className="mb-2">
-                    A software engineer with a passion in ui and front end
-                    development
-                  </div>
-                  <div className="flex mb-2 max-md:justify-center">
-                    <FaGithub className="text-2xl me-3" />
-                    <FaLinkedin className="text-2xl" />
-                  </div>
-                </div>
-
               </div>
-              
-              <div className="">
-                <div className=" text-wrap">
+              <div className="flex justify-center items-center">
+                <div className="max-sm:text-center">
                   <div className="mb-2">
-                    Bachelor of Science in Informationa and Technology. My
-                    interest in web development sparked during college, where I
-                    discovered a passion for building functional web applications.
-                  </div>
-                  <div className="mb-2">
-                    Continuously seeking to enhance my skills, I work on personal
-                    projects using a range of technologies. These projects not
-                    only serve as a creative outlet but also as a means to stay
-                    up-to-date with the latest trends and tools in web
-                    development.
+                    <h2 className=" text-[27px] font-bold">
+                      Mohammad Jul Hafiz Arola
+                    </h2>
+                    <h2 className="text-xl mb-5">
+                      Web Developer and Character Design.
+                    </h2>
+                    <div className="flex flex-wrap justify-center">
+                      <div className="flex me-3">
+                        <a href="" className="flex">
+                          <FaGithub className="text-2xl me-2"/>
+                          <h2>Github</h2>
+                        </a>
+                      </div>
+                      <div className="flex me-3">
+                        <a href="" className="flex">
+                          <FaLinkedin className="text-2xl me-2"/>
+                          <h2>LinkedIn</h2>
+                        </a>
+                      </div>
+                      <div className="flex me-3">
+                        <a href="" className="flex">
+                          <FaTwitter className="text-2xl me-2"/>
+                          <h2>Twitter</h2>
+                        </a>
+                      </div>
+                      <div className="flex">
+                        <a href="" className="flex">
+                          <FaInstagram className="text-2xl me-2"/>
+                          <h2>Instagram</h2>
+                        </a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
