601bc6a2d86a997e4911b1610e0a3c704ef86fc6
hydrotolerance <julzhafiz@gmail.com>
2024-05-08 18:25:50 +0800

    (Fix) Filter

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -6,14 +6,14 @@ function Layout({children}: MyComponentProps) {
   
   return (
     <>
-          <svg className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light" width="100%" height="100%">
+          <svg className="pointer-events-none fixed isolate z-50 opacity-70 mix-blend-soft-light h-screen w-full">
             <filter id="noise">
             <feTurbulence type="fractalNoise" baseFrequency="0.80" numOctaves="4" stitchTiles="stitch"></feTurbulence>
             </filter>
-            <rect width="100%" height="100%" filter="url(#noise)"></rect>
+            <rect className='h-screen w-full' filter="url(#noise)"></rect>
           </svg>
         <Header/>
-        <div className='w-auto h-auto'>{children}</div>
+        <div>{children}</div>
 
     </>
 
